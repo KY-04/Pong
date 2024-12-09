265287bfb521f05b304c01ff641d506f1bb0a3f1
Kai_030 <kekengilbor@gmail.com>
2024-12-09 19:52:46 +1100

    Added New Features (Sound Button)
    - Added The Sound Button and Slider
    - Added the script
    - Fixed the Option and Credit Panel

---
+++ Assets/Scripts/VolumeSlider.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+//https://youtu.be/yWCHaTwVblk?si=EJu5BXZyyK783sP4
+
+public class VolumeSlider : MonoBehaviour
+{
+    [SerializeField] Slider Volume_Slider;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("Music"))
+        {
+            PlayerPrefs.SetFloat("Music", 1);
+            LoadVolume();
+        }
+
+        else
+        {
+            LoadVolume();
+        }
+    }
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = Volume_Slider.value;
+        SaveVolume();
+    }
+
+    private void LoadVolume()
+    {
+        Volume_Slider.value = PlayerPrefs.GetFloat("Music");
+    }
+
+    private void SaveVolume()
+    {
+        PlayerPrefs.SetFloat("Music", Volume_Slider.value);
+    }
+}
